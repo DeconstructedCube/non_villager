fa3c302e3d2658a307d6d6636e4e2ffc77e234c2
bsfdsagfadg <918272281@qq.com>
2026-08-03 19:57:18 +0800

    Remove unused template mixins, client entrypoint, and dead code

--- src/main/java/com/bsfdsagfadg/non_villager/NonVillagerMod.java
+++ src/main/java/com/bsfdsagfadg/non_villager/NonVillagerMod.java
@@ -2,30 +2,17 @@ package com.bsfdsagfadg.non_villager;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.minecraft.resources.Identifier;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class NonVillagerMod implements ModInitializer {
 	public static final String MOD_ID = "non_villager";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
 		ModProfessions.register();
 		LOGGER.info("NoN Villager Professions initialized.");
-	}
-
-	public static Identifier id(String path) {
-		return Identifier.fromNamespaceAndPath(MOD_ID, path);
 	}
 }
